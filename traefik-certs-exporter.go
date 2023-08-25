1366a6f57fb813f05cd28853ec1002d2120fb1f5
Vladimir nett00n Budylnikov <git@nett00n.org>
2023-08-25 05:17:52 +0000

    optimize code

--- traefik-certs-exporter.go
+++ traefik-certs-exporter.go
@@ -7,89 +7,90 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"path/filepath"
 )
 
-// ACME is an imported global traefik acme.json sctructure
+// ACME is an imported global traefik acme.json structure
 type ACME struct {
 	Le struct {
-		Account struct {
-			Email string
-		} `json: "Account"`
+		Account      Account
 		Certificates []Certificates
-	} `json: "le"`
+	} `json:"le"`
+}
+
+// Account represents the account details in acme.json
+type Account struct {
+	Email string
 }
 
 // Certificates list in acme.json
 type Certificates struct {
-	Domain struct {
-		Main string `json: "main"`
-	} `json: "domain"`
-	Certificate string `json: "certificate"`
-	Key         string `json: "key"`
+	Domain      Domain
+	Certificate string `json:"certificate"`
+	Key         string `json:"key"`
+}
+
+// Domain represents the domain details in acme.json
+type Domain struct {
+	Main string `json:"main"`
 }
 
 func main() {
-	// open json file
+	// Open json file
 	jsonFile, err := os.Open("input/acme.json")
-	// if os.Open returns an error then print out it
 	if err != nil {
-		log.Println(err)
+		log.Fatal(err)
 	}
-	log.Println("Successfully Opened acme.json")
-	// defer the closing of the json file
 	defer jsonFile.Close()
 
-	// create directory for certs
-	path := "output"
-	err = os.MkdirAll(path, os.ModePerm)
+	log.Println("Successfully Opened acme.json")
+
+	// Read JSON content
+	byteValue, err := ioutil.ReadAll(jsonFile)
 	if err != nil {
-		log.Println(err)
+		log.Fatal(err)
 	}
 
-	byteValue, _ := ioutil.ReadAll(jsonFile)
 	var traefik ACME
-	err = json.Unmarshal(byteValue, &traefik)
-	if err != nil {
-		panic(err)
+	if err := json.Unmarshal(byteValue, &traefik); err != nil {
+		log.Fatal(err)
 	}
-	for i := 0; i < len(traefik.Le.Certificates); i++ {
-		// print cert hostname
-		log.Println("Certificate host:", traefik.Le.Certificates[i].Domain.Main)
-		// decode and print certificate
-		certBody, err := base64.StdEncoding.DecodeString(traefik.Le.Certificates[i].Certificate)
-		if err != nil {
-			log.Println(err)
-		}
-		log.Println("Save certificate body", traefik.Le.Certificates[i].Domain.Main)
-		// create cert file
-		certFile, err := os.Create("output/" + traefik.Le.Certificates[i].Domain.Main + ".cer")
-		if err != nil {
-			log.Println(err)
-		}
-		defer certFile.Close()
 
-		_, err = certFile.Write(certBody)
+	// Create output directory if not exists
+	outputDir := "output"
+	if err := os.MkdirAll(outputDir, os.ModePerm); err != nil {
+		log.Fatal(err)
+	}
+
+	for _, cert := range traefik.Le.Certificates {
+		// Print certificate hostname
+		log.Println("Certificate host:", cert.Domain.Main)
+
+		// Decode certificate and key
+		certBody, err := base64.StdEncoding.DecodeString(cert.Certificate)
 		if err != nil {
 			log.Println(err)
+			continue
 		}
 
-		// decode and print key
-		certKey, err := base64.StdEncoding.DecodeString(traefik.Le.Certificates[i].Key)
+		certKey, err := base64.StdEncoding.DecodeString(cert.Key)
 		if err != nil {
 			log.Println(err)
+			continue
 		}
-		log.Println("Save certificate key", traefik.Le.Certificates[i].Domain.Main)
-		// create cert file
-		keyFile, err := os.Create("output/" + traefik.Le.Certificates[i].Domain.Main + ".key")
-		if err != nil {
-			log.Println(err)
-		}
-		defer keyFile.Close()
 
-		_, err = keyFile.Write(certKey)
-		if err != nil {
+		// Create certificate file
+		certPath := filepath.Join(outputDir, cert.Domain.Main+".cer")
+		if err := ioutil.WriteFile(certPath, certBody, os.ModePerm); err != nil {
 			log.Println(err)
 		}
 
+		// Create key file
+		keyPath := filepath.Join(outputDir, cert.Domain.Main+".key")
+		if err := ioutil.WriteFile(keyPath, certKey, os.ModePerm); err != nil {
+			log.Println(err)
+		}
+
+		log.Println("Saved certificate and key for", cert.Domain.Main)
 	}
 }
